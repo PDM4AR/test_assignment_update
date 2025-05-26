546fa17c4012520eba2dbc69db650b0e69be8e9e
Yueshan Li <yuesli@ethz.ch>
2025-05-26 13:55:30 +0200

    Update ex1.py

--- src/pdm4ar/exercises/ex01/ex1.py
+++ src/pdm4ar/exercises/ex01/ex1.py
@@ -21,4 +21,5 @@ def compare_lexicographic(a: Tuple[float], b: Tuple[float]) -> ComparisonOutcome
     but more important than the one in position 2
     """
     # todo: implement the comparison
+    bbbbbbbbbbbbbbbbb
     return INDIFFERENT
